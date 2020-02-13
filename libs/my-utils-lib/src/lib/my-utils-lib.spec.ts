5e62a45bf237d4e2ca5e0d60a32c8c20ccb45ef6
Graeme Norman <Graeme.Norman@genistar.net>
2020-02-13 10:41:14 +0000

    Add my-utils library (1st sharable library to this monorepo)

---
+++ libs/my-utils-lib/src/lib/my-utils-lib.spec.ts
@@ -0,0 +1,7 @@
+import { myUtilsLib } from './my-utils-lib';
+
+describe('myUtilsLib', () => {
+  it('should work', () => {
+    expect(myUtilsLib()).toEqual('my-utils-lib');
+  });
+});
---
+++ libs/my-utils-lib/src/lib/my-utils-lib.ts
@@ -0,0 +1,5 @@
+export function myUtilsLib(): string {
+  return 'my-utils-lib';
+}
+
+export const sharedTitleString = 'Welcome to Genistar!';
